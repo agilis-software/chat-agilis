01b49a6a76e923020649039d4785f2232f2774ba
smokeeaasd <lucasalencargomes111@gmail.com>
2024-11-30 23:06:28 -0300

    refactor: config

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ dotenv.config()
 const config = {
   host: process.env.HOST || 'localhost',
   port: process.env.PORT || 3000,
-  debug: !process.env.DEBUG || process.env.DEBUG === 'true',
+  debug: process.env.DEBUG || true,
   secret: process.env.CHAT_SERVER_SECRET || 'CHAT_SERVER_SECRET',
 }
 
